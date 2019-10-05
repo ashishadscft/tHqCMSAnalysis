87ec81214e1d9c11a789ab2254e1cfbeff2703f4
hiram <hiram.e.damian@gmail.com>
2019-10-05 10:56:27 -0700

    algo

--- fit_2lss_2016/eps.h
+++ fit_2lss_2016/eps.h
@@ -3,4 +3,4 @@
 #define eps_fakes 9.4/80.9 //sqrt(pow(0.5,2)+pow(0.07,2))
 #define  eps_ttz 3.9/25.9 //sqrt(pow(0.04,2)+pow(0.1,2)+pow(0.01,2) +pow(0.06,2))
 #define  eps_wz  7.7/15.1 //sqrt(pow(0.5,2)+pow(0.06,2)+pow(0.01,2))
-#define eps_ttw 10.0/68.0 // sqrt(pow(0.04,2)+pow(0.12,2)+pow(0.01,2) +pow(0.06,2))
+#define eps_ttw 10.0/68.0 // sqrt(pow(0.04,2)+pow(0.12,2)+pow(0.01,2
