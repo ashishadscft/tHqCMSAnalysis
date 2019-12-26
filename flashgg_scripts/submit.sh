bc9b17b560981cbc2102cfcd16cf6a644935e919
Jose <jose.benitez@cern.ch>
2019-12-27 00:05:44 +0100

    Fix

--- flashgg_scripts/submit.sh
+++ flashgg_scripts/submit.sh
@@ -1,5 +1,6 @@
 
 INSTALLDIR=/afs/cern.ch/user/b/benitezj/output/tH/workspace/CMSSW_10_5_0/src
 
-fggRunJobs.py --load ${INSTALLDIR}/tHqCMSAnalysis/flashgg_scripts/higgs_2016.json -d higgs_2016 -n 300 -q workday --no-copy-proxy ${INSTALLDIR}/flashgg/Systematics/test/workspaceStd.py 
+#fggRunJobs.py --load ${INSTALLDIR}/tHqCMSAnalysis/flashgg_scripts/higgs_2016.json -d higgs_2016 -n 300 -q workday --no-copy-proxy ${INSTALLDIR}/flashgg/Systematics/test/workspaceStd.py 
+fggRunJobs.py --load ${INSTALLDIR}/tHqCMSAnalysis/flashgg_scripts/higgs_2016_v2.json -d higgs_2016_v2 -n 300 -q workday --no-copy-proxy ${INSTALLDIR}/flashgg/Systematics/test/workspaceStd.py 
 #fggRunJobs.py  --load ${INSTALLDIR}/tHqCMSAnalysis/flashgg_scripts/thq_2016.json -d thq_2016  ${INSTALLDIR}/flashgg/Systematics/test/workspaceStd.py  -n 300  -q workday  --no-copy-proxy 
