e2e62c2b1c1475fbef39ddc53f640eb020bd12fc
Jose <jose.benitez@cern.ch>
2019-12-30 02:00:17 +0100

    2018 samples

--- flashgg_scripts/submit.sh
+++ flashgg_scripts/submit.sh
@@ -10,5 +10,10 @@ INSTALLDIR=/afs/cern.ch/user/b/benitezj/output/tH/workspace/CMSSW_10_5_0/src
 ###2017
 #fggRunJobs.py  --load ${INSTALLDIR}/tHqCMSAnalysis/flashgg_scripts/thq_2017.json -d thq_2017  ${INSTALLDIR}/flashgg/Systematics/test/workspaceStd.py  -n 300  -q workday  --no-copy-proxy 
 #fggRunJobs.py  --load ${INSTALLDIR}/tHqCMSAnalysis/flashgg_scripts/higgs_2017.json -d higgs_2017  ${INSTALLDIR}/flashgg/Systematics/test/workspaceStd.py  -n 300  -q workday  --no-copy-proxy 
+#fggRunJobs.py  --load ${INSTALLDIR}/tHqCMSAnalysis/flashgg_scripts/data_2017.json -d data_2017  ${INSTALLDIR}/flashgg/Systematics/test/workspaceStd.py  -n 300  -q workday  --no-copy-proxy 
 
-fggRunJobs.py  --load ${INSTALLDIR}/tHqCMSAnalysis/flashgg_scripts/data_2017.json -d data_2017  ${INSTALLDIR}/flashgg/Systematics/test/workspaceStd.py  -n 300  -q workday  --no-copy-proxy 
+
+###2018
+#fggRunJobs.py  --load ${INSTALLDIR}/tHqCMSAnalysis/flashgg_scripts/thq_2018.json -d thq_2018  ${INSTALLDIR}/flashgg/Systematics/test/workspaceStd.py  -n 300  -q workday  --no-copy-proxy 
+#fggRunJobs.py  --load ${INSTALLDIR}/tHqCMSAnalysis/flashgg_scripts/higgs_2018.json -d higgs_2018  ${INSTALLDIR}/flashgg/Systematics/test/workspaceStd.py  -n 300  -q workday  --no-copy-proxy 
+fggRunJobs.py  --load ${INSTALLDIR}/tHqCMSAnalysis/flashgg_scripts/data_2018.json -d data_2018  ${INSTALLDIR}/flashgg/Systematics/test/workspaceStd.py  -n 300  -q workday  --no-copy-proxy 
